d82fe237b21611b570bd7aa0386fb9d634864a37
Victor Lee <victorlhlee@users.noreply.github.com>
2019-06-03 11:35:02 -1000

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -1,5 +1,5 @@
 // Function -  FirstFactorial(num)
-// The function will take the num parameter being passed and return the factorial of it (ie. if num =3, return(3 * 2 * 1)).  For test cases, the range will be between 1 and 18.
+// The function will take the num parameter being passed and return the factorial of it (ie. if num = 3, return 6).  For test cases, the range will be between 1 and 18.
 
 // Function - SimpleAdding(num)
 // The function will take the num parameter and add up all the numbers from 1 to num.  For test cases, the parameter num will be any number from 1 to 1000.
